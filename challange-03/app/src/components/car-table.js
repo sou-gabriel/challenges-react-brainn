53d0ec60c9d54382c0756755c8ed899d6efbbc87
Gabriel <dev.gabrielramos@gmail.com>
2021-09-06 21:37:57 -0300

    Challange 03 - Exercise 01

---
+++ challange-03/app/src/components/car-table.js
@@ -0,0 +1,61 @@
+export const CarTable = ({ cars, setCars }) => {
+  const deleteCar = (event) => {
+    fetch('http://localhost:3333/cars', {
+      method: 'DELETE',
+      headers: {
+        'content-type': 'application/json',
+      },
+      body: JSON.stringify({
+        plate: event.target.dataset.plate,
+      }),
+    })
+
+    const filteredCars = cars.filter(
+      (car) => car.plate !== event.target.dataset.plate
+    )
+
+    setCars(filteredCars)
+  }
+
+  return (
+    <div className="table-wrapper">
+      <table className="table">
+        <thead className="table-head">
+          <tr>
+            <th className="cell">Imagem (URL)</th>
+            <th className="cell">Marca/Modelo</th>
+            <th className="cell">Ano</th>
+            <th className="cell">Placa</th>
+            <th className="cell">Cor</th>
+            <th className="cell"></th>
+          </tr>
+        </thead>
+
+        <tbody className="table-content">
+          {cars.length ? (
+            cars.map((car) => (
+              <tr className="row" key={car.plate}>
+                <td className="cell">
+                  <img alt={car.brandModel} width="200" src={car.image} />
+                </td>
+                <td className="cell">{car.brandModel}</td>
+                <td className="cell">{car.year}</td>
+                <td className="cell">{car.plate}</td>
+                <td className="cell">{car.color}</td>
+                <td className="cell">
+                  <button className="button-delete" data-plate={car.plate} onClick={deleteCar}>
+                    Deletar
+                  </button>
+                </td>
+              </tr>
+            ))
+          ) : (
+            <tr>
+              <td colSpan="5" className="cell centralized">Nenhum carro encontrado</td>
+            </tr>
+          )}
+        </tbody>
+      </table>
+    </div>
+  )
+}
